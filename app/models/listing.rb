cae3c3726c1968bc55eb1f8a1876ac75e2e6997c
Glenn Francis <XglennfrancisX@gmail.com>
2018-11-01 13:04:47 +1000

    implemented cloudinary images

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,19 @@
+require 'cloudinary'
+
 class Listing < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
   validates :subject, presence: true
   has_many :comments
   has_one_attached :image
+
+  def image_tag
+    cl_image_tag(self.image.key, :width=>730, :crop=>"fit")
   end
+
+  def thumb_tag
+    cl_image_tag(self.image.key, :width=>300, :crop=>"fit")
+  end
+
+
+end
